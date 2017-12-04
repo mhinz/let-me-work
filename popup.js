d1809ad6b1965f07f326af52a7e36a2fcda0e46d
Marco Hinz <mh.codebro@gmail.com>
2017-12-04 19:05:41 +0100

    Remove from storage and popup

--- popup.js
+++ popup.js
@@ -1,17 +1,36 @@
 let arr = ['foo', 'bar', 'quux'];
 chrome.storage.sync.set({"blacklist": arr});
 
+function removeFromStorage(blacklist, pattern) {
+  chrome.storage.sync.set({'blacklist': blacklist.filter(e => e !== pattern)});
+}
+
+function removeFromPopup(pattern) {
+  let table = document.getElementsByTagName('table')[0];
+  for (let i = 0; i < table.rows.length; i++) {
+    if (table.rows[i].cells[0].innerHTML === pattern) {
+      table.deleteRow(i);
+      return;
+    }
+  }
+}
+
 function createTable() {
   chrome.storage.sync.get('blacklist', (items) => {
-    window.console.log(items);
     let table = document.getElementsByTagName('table')[0];
 
-    for (i = 0; i < items.blacklist.length; i++) {
+    for (let i = 0; i < items.blacklist.length; i++) {
       let row = table.insertRow(i);
       let cell = row.insertCell(0);
       cell.width = '100%';
       cell.innerHTML = items.blacklist[i];
-      row.insertCell(1).innerHTML = '<button type="button">dump</button>'
+      let button = document.createElement('button');
+      button.innerHTML = 'dump';
+      button.addEventListener('click', () => {
+        removeFromPopup(items.blacklist[i]);
+        removeFromStorage(items.blacklist, items.blacklist[i]);
+      });
+      row.insertCell(1).appendChild(button);
     }
   });
 }

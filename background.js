09f3b1c66426abf7e0d424eed57454f1097756e0
Marco Hinz <mh.codebro@gmail.com>
2017-12-04 03:14:58 +0100

    init

---
+++ background.js
@@ -0,0 +1,14 @@
+let blacklist = [
+  "https?://twitter.com/.*"
+]
+
+let pattern = new RegExp(blacklist.join('|'));
+
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    if (typeof changeInfo.url !== 'undefined' && pattern.test(changeInfo.url)) {
+      console.log('Oh noes.. Twitter.');
+      chrome.tabs.update(tabId, {url: chrome.extension.getURL('focus.html')});
+    }
+  }
+);
